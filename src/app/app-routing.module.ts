bae40ce85d7a3eb26d680fb75b50f2bb14bb04ef
Myriam MIRA <myriam.mira@avanade.com>
2022-10-31 18:01:28 +0100

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GalerieComponent } from './galerie/galerie.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'galerie', component: GalerieComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
